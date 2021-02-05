5c261e9108e0a7cd4f1fca07075246c888235875
the42game@code <theskanthunt42@gmail.com>
2021-02-05 11:27:24 +0800

    Updating

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class simpQRTool(QtWidgets.QMainWindow, mainUi.Ui_MainWindow):
         super(simpQRTool, self).__init__(parent)
         self.systemPlatform = sys.platform #Get current system POSIX or Windows or sth else
         self.setupUi(self)
+        self.actionExit.triggered.connect(self.exit)
         self.encodingUnicode8.setChecked(True) #Default value
         self.res172.setChecked(True)
         self.modeTextToQR.setChecked(True)
@@ -22,13 +23,17 @@ class simpQRTool(QtWidgets.QMainWindow, mainUi.Ui_MainWindow):
         self.buttonGroupRes.addButton(self.res480)
         self.buttonGroupRes.addButton(self.res720)
         self.modeQRToText.toggled.connect(self.modeDecode) #Toggle connect
-        self.modeTextToQR.toggle.connect(self.modeEncode)
+        self.modeTextToQR.toggled.connect(self.modeEncode)
         self.encodingUnicode8.toggled.connect(self.encodeModeUni)
         self.encodingAscii.toggled.connect(self.encodeModeAscii)
         self.encodingShift.toggled.connect(self.encodeModeShift)
         self.res172.toggled.connect(self.resSet172)
         self.res480.toggled.connect(self.resSet480)
         self.res720.toggled.connect(self.resSet720)
+        self.mainExec.clicked.connect(self.convertTrigger)
+        self.importExec.clicked.connect(self.importTrigger)
+        self.exportExec.clicked.connect(self.exportMain)
+        self.actionExport.triggered.connect(self.exportMain)
 
 #This function below is use to output infomations to Terminal or the statubar of the GUI
     def infoOutput(self, logs, terminal, statBar, statBarTime):
@@ -42,4 +47,42 @@ class simpQRTool(QtWidgets.QMainWindow, mainUi.Ui_MainWindow):
         else:
             pass
 
-    
+    def exit(self):
+        self.infoOutput(logs='Exit triggered.', terminal=True, statBar=False, statBarTime=0)
+        raise SystemExit
+#Use to handle mode switch
+    def modeDecode(self):
+        #Disable all widgets that wont need
+        radioButton = self.sender()
+        if radioButton.isChecked():    
+            self.currentMode = "Q2T"
+            #self.mainExec.setEnabled(False)
+            self.exportExec.setEnabled(False)
+            self.encodingUnicode8.setEnabled(False)
+            self.encodingAscii.setEnabled(False)
+            self.encodingShift.setEnabled(False)
+            self.res172.setEnabled(False)
+            self.res480.setEnabled(False)
+            self.res720.setEnabled(False)
+#Same as up
+    def modeEncode(self):
+        #Enable all stuff incase some one select "QR to Text" before
+        #self.mainExec.setEnabled(True)
+        radioButton = self.sender()
+        if radioButton.isChecked():
+            self.currentMode = "T2Q"
+            self.exportExec.setEnabled(True)
+            self.encodingShift.setEnabled(True)
+            self.encodingUnicode8.setEnabled(True)
+            self.encodingAscii.setEnabled(True)
+            self.res172.setEnabled(True)
+            self.res480.setEnabled(True)
+            self.res720.setEnabled(True)
+
+def main():
+    app = QApplication(sys.argv)
+    form = simpQRTool()
+    form.show()
+    app.exec_()
+if __name__ == '__main__':
+    main()
